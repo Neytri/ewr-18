a22b6c85ceae2f4aac041b365ce0578e3e40ed37
Marcel Parpart <subbyberlin@googlemail.com>
2018-05-15 03:37:53 +0200

    erste lösungssuche

--- uebung_2a2_einmaleins.py
+++ uebung_2a2_einmaleins.py
@@ -4,4 +4,20 @@ Es wird eine Zahl zwischen 0 und 20 eingegeben. Das Programm gibt die
 Einmaleins-Tabelle fur diese Zahl aus.
 '''
 
+TABELLENGROESSE = int(input("Wie weit hätten Sie gern die Tabelle: "))
 
+for i in range(1, TABELLENGROESSE):
+    print("{:3d} {:4d} {:5d}".format(i, i*i, i*i*i))
+
+line = ""
+n = TABELLENGROESSE
+
+# tabelle zeichnen
+for i in range(0, n + 1):
+    line += "{:>3} |".format(i+1)
+for i in range(0, n + 1):
+    line += "----|"
+for i in range(0, n + 1):
+    line += "{:>3} |".format(i*i)
+
+print(line)
