0ada0d2351bc95f6a2f8a44d1218176a62096af2
Marcel Parpart <subbyberlin@googlemail.com>
2018-05-15 06:17:25 +0200

    add eingabenmaximum, lösung unvollständig

--- uebung_2a2_einmaleins.py
+++ uebung_2a2_einmaleins.py
@@ -5,19 +5,21 @@ Einmaleins-Tabelle fur diese Zahl aus.
 '''
 
 TABELLENGROESSE = int(input("Wie weit hätten Sie gern die Tabelle: "))
-
-for i in range(1, TABELLENGROESSE):
-    print("{:3d} {:4d} {:5d}".format(i, i*i, i*i*i))
-
-line = ""
-n = TABELLENGROESSE
+LINE = ""
+zahl = ""
+### Stringformatierung durch str.format() und platzhaltern
+#for i in range(1, TABELLENGROESSE):
+#    print("{:3d} {:4d} {:5d}".format(i, i*i, i*i*i))
+if TABELLENGROESSE > 20:
+    TABELLENGROESSE = 20
+print("20 ist das Maximum und wurde gesetzt.")
 
 # tabelle zeichnen
-for i in range(0, n + 1):
-    line += "{:>3} |".format(i+1)
-for i in range(0, n + 1):
-    line += "----|"
-for i in range(0, n + 1):
-    line += "{:>3} |".format(i*i)
+for i in range(0, TABELLENGROESSE + 1):
+    LINE += "{:4d} |".format(i)
+    print(LINE)
 
-print(line)
+for j in range(0, TABELLENGROESSE + 1):
+    for k in range(0, TABELLENGROESSE +1):
+        zahl += "{:4d} |".format(j*k)
+    print(zahl)
